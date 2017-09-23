39c58adfc23d2248b255b945b3f1c2399be2dad0
Andrew Lincoln <andrewjlincoln@gmail.com>
2017-09-23 19:48:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ function viewCart() {
 
     for (var c = 0; c < cart.length; c++) {
       var item = Object.keys(cart[c])
-      var priceGrabber = cart[c]
-      var price = priceGrabber.item
+      //var priceGrabber = item
+      var price = item[item]
 
       if (c === 0) {
         cartList = cartList + `${item} at $${price}.`
